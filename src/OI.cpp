9f62aea3d64b9935d1499713336514545a6e808c
a_kitkat <ankbhatia19@gmail.com>
2018-01-22 08:40:54 -0800

    OI
    Finished OI

--- src/OI.cpp
+++ src/OI.cpp
@@ -2,6 +2,24 @@
 
 #include <WPILib.h>
 
-OI::OI() {
+OI::OI()
+:leftJoystick(new Joystick(LEFT_JOY_PORT)), rightJoystick(new Joystick(RIGHT_JOY_PORT))
+{
 	// Process operator interface input here.
+}
+
+OI::~OI()
+{
+	delete leftJoystick;
+	delete rightJoystick;
+}
+
+Joystick * OI::getLeftJoystick()
+{
+	return leftJoystick;
+}
+
+Joystick * OI::getRightJoystick()
+{
+	return rightJoystick;
 }
--- src/OI.h
+++ src/OI.h
@@ -1,9 +1,22 @@
 #ifndef OI_H
 #define OI_H
+#include <WPILib.h>
 
 class OI {
 public:
 	OI();
+	~OI();
+	Joystick * getLeftJoystick();
+	Joystick * getRightJoystick();
+
+private:
+
+	Joystick * leftJoystick;
+	Joystick * rightJoystick;
+	const int LEFT_JOY_PORT = 0;
+	const int RIGHT_JOY_PORT = 1;
 };
+
+
 
 #endif  // OI_H
